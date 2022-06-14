0692cb063bb89e145e2b51c47a6d8aa051e2f2f8
Riya Prasad <prasadriya79@gmail.com>
2022-06-14 20:47:26 +0530

    change link address and animation

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ const sr = ScrollReveal({
 })
 
 
-sr.reveal(`.hero h1, .hero p, .about h1, .about .btn, .work .card, .contact-page-link`,{
+sr.reveal(`.hero h1, .hero p, .about h1, .about p .about .btn, .work .card, .contact .start-project, .contact-page-link`,{
     origin: 'top',
     interval: 100,
 })
 
-sr.reveal(`.navbar .logo,.hero .svg,.about p, .about .flex h2, .contact .start-project, .contact h1, .about span`,{
+sr.reveal(`.navbar .logo,.hero .svg, .about .flex h2,  .contact h1, .about span`,{
     origin: 'left',
     interval: 100,
 })
